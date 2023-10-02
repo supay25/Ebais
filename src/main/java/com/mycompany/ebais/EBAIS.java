32088877cd9db7962bc526e0c028346abded9b48
Jose <Jose@DESKTOP-UB9REI4.mshome.net>
2023-10-01 17:23:53 -0700

    pene

--- src/main/java/com/mycompany/ebais/EBAIS.java
+++ src/main/java/com/mycompany/ebais/EBAIS.java
@@ -4,6 +4,9 @@
 
 package com.mycompany.ebais;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author moise_oueu1ax
@@ -11,6 +14,22 @@ package com.mycompany.ebais;
 public class EBAIS {
 
     public static void main(String[] args) {
+        
+        
+        ConDoctor cp1 = new ConDoctor();
+        Doctor d = new Doctor();
+        
+        Paciente p = new Paciente();
+        d.setEspecialidad("Ontologia");
+        d.setNombre("Jose");
+        
+        
+        cp1.insertar(d);
+        List<Doctor> listaRetornoo = cp1.lista();
+        for (Doctor pa : listaRetornoo){
+            System.out.print(pa.getEspecialidad());
+        }
+        
        
         
         
