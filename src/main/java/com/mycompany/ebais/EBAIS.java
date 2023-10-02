258f69f6721abcdff50abaf46077c7c894a039b2
Jose <Jose@DESKTOP-UB9REI4.mshome.net>
2023-10-01 18:42:06 -0700

    que lindos mis companeros uwu

--- src/main/java/com/mycompany/ebais/EBAIS.java
+++ src/main/java/com/mycompany/ebais/EBAIS.java
@@ -18,6 +18,7 @@ public class EBAIS {
         
         ConDoctor cp1 = new ConDoctor();
         Doctor d = new Doctor();
+        ConExpediente ex = new ConExpediente();
         
         Paciente p = new Paciente();
         d.setEspecialidad("Ontologia");
@@ -26,12 +27,29 @@ public class EBAIS {
         
         cp1.insertar(d);
         List<Doctor> listaRetornoo = cp1.lista();
-        for (Doctor pa : listaRetornoo){
-            System.out.print(pa.getEspecialidad());
-        }
-        
+        //for (Doctor pa : listaRetornoo){
+          //  System.out.print(pa.getEspecialidad());
+        //}
+        Expediente e = new Expediente();
+        e.setComentario("Le falta drogas");
+        e.setPaciente(5);
+        ex.insertar(e);
+      /* Consultorio con = new Consultorio();
+       con.setDireccion("Condominio Rio de Oro");
        
-        
-        
+       ConConsultorios concon = new ConConsultorios();
+       concon.insertar(con);
+       
+       List<Consultorio> listaRetornoCon = concon.lista();
+       for (Consultorio pa : listaRetornoCon){
+            System.out.print(pa.getDireccion());
+        }*/
+       Citas ci = new Citas();
+       ConCitas conci = new ConCitas();
+       ci.setConsultorio(1);
+       ci.setPaciente(1);
+       ci.setDoctor(1);
+       conci.insertar(ci);
+      
     }
 }
